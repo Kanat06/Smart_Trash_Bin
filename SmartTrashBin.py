6aaabf70535f8134b665a344882d2f2677589040
Kanat06 <122548557+Kanat06@users.noreply.github.com>
2023-11-09 21:53:26 +0600

    SmartTrashBin.py

--- SmartTrashBin.py
+++ SmartTrashBin.py
@@ -15,7 +15,6 @@ IN02 = 22  # GPIO pin connected to IN2 of ULN2003
 IN03 = 24  # GPIO pin connected to IN3 of ULN2003
 IN04 = 26 
 
-
 # Set up GPIO pins as outputs
 GPIO.setup(IN1, GPIO.OUT)
 GPIO.setup(IN2, GPIO.OUT)
@@ -26,9 +25,6 @@ GPIO.setup(IN01, GPIO.OUT)
 GPIO.setup(IN02, GPIO.OUT)
 GPIO.setup(IN03, GPIO.OUT)
 GPIO.setup(IN04, GPIO.OUT)
-
-
-
 
 # Define the motor control sequences
 step_sequence = [
@@ -45,18 +41,21 @@ step_sequence2 = [
     [0, 0, 0, 1]
 ]
 
+# Function to set motor step
 def set_step(step):
     GPIO.output(IN1, step[0])
     GPIO.output(IN2, step[1])
     GPIO.output(IN3, step[2])
-    GPIO.output(IN4, step[3])
-    
+    GPIO.output(IN4, step[3]
+
+# Function to set motor2 step
 def set_step2(step):
     GPIO.output(IN01, step[0])
     GPIO.output(IN02, step[1])
     GPIO.output(IN03, step[2])
-    GPIO.output(IN04, step[3])
+    GPIO.output(IN04, step[3]
 
+# Function to rotate motor
 def rotate_motor(steps, delay):
     if steps > 0:
         for _ in range(steps):
@@ -68,7 +67,8 @@ def rotate_motor(steps, delay):
             for step in reversed(step_sequence):
                 set_step(step)
                 time.sleep(delay)
-                
+
+# Function to rotate motor2
 def rotate_motor0(steps, delay):
     if steps > 0:
         for _ in range(steps):
@@ -79,9 +79,7 @@ def rotate_motor0(steps, delay):
         for _ in range(abs(steps)):
             for step in reversed(step_sequence2):
                 set_step2(step)
-                time.sleep(delay)                
-
-
+                time.sleep(delay)
 
 cap = cv2.VideoCapture(0)
 cr = Classifier('keras_model.h5', 'labels.txt')
@@ -90,151 +88,44 @@ try:
     # Initial 360-degree rotation to the right
     rotate_motor(steps=500, delay=0.005)
     rotate_motor(steps=-142, delay=0.005)
-     
-      
 
     while True:
-        
+        # Capture video frame from the camera
         ret, frame = cap.read()
         frame = cv2.resize(frame, (640, 480))
-        pd = cr.getPrediction(frame)
-        
-        cv2.imshow("Frame", frame)
-        
-        cv2.waitKey(1)
-        
-       
-        if pd[1] != 5:
-            
-            if pd[1] == 0:
-                
-                
-                
-                rotate_motor(-130, 0.005)
-                
-                rotate_motor0(69, 0.005)
-                rotate_motor0(-69, 0.005)
-                
-                rotate_motor(130, 0.005)
-                  
-                time.sleep(4)
-                ret, stored_frame = cap.read()
-                stored_frame = cv2.resize(stored_frame, (640, 480))
-                
-                cv2.imshow("Frame", stored_frame)
-                cv2.waitKey(1)
-                for _ in range(3):
-                    
-                    cap.read()
-               # stored_frame = frame.copy()
-                #if stored_frame is not None:
-                 
-            
-                    
-                 #   ret, frame = cap.read()
-                  #  frame = cv2.resize(frame, (640, 480))
-                   # cv2.imshow("Frame", frame)
-                    #cv2.waitKey(1)  # Refresh the OpenCV window
-                
-            elif pd[1] == 1:
-                
-                
-                
-                rotate_motor(-60, 0.005)
-                
-                rotate_motor0(69, 0.005)
-                rotate_motor0(-69, 0.005)
-                
-                rotate_motor(60, 0.005)
-                time.sleep(4)
-                
-                ret, stored_frame = cap.read()
-                stored_frame = cv2.resize(stored_frame, (640, 480))
-                
-                cv2.imshow("Frame", stored_frame)
-                cv2.waitKey(1)
-                for _ in range(3):
-                    
-                    cap.read()
-                
-            elif pd[1] == 2:
-                
-                
-                
-                rotate_motor(20, 0.005)
-                
-                rotate_motor0(69, 0.005)
-                rotate_motor0(-69, 0.005)
-                
-                rotate_motor(-20, 0.005)  
-                time.sleep(4)
-                ret, stored_frame = cap.read()
-                stored_frame = cv2.resize(stored_frame, (640, 480))
-                
-                cv2.imshow("Frame", stored_frame)
-                cv2.waitKey(1)
-                for _ in range(3):
-                    
-                    cap.read()
-                
-            elif pd[1] == 3:
-                
-                
-                
-                rotate_motor(140, 0.005)
-                
-                rotate_motor0(69, 0.005)
-                rotate_motor0(-69, 0.005)
-                
-                rotate_motor(-140, 0.005)
-                time.sleep(4)
-                
-                stored_frame = frame.copy()                
-                ret, stored_frame = cap.read()
-                stored_frame = cv2.resize(stored_frame, (640, 480))
-                
-                cv2.imshow("Frame", stored_frame)
-                cv2.waitKey(1)
-                for _ in range(3):
-                    
-                    cap.read()
-                
-            elif pd[1] == 4:
-                
-                
-                
-                rotate_motor(-250, 0.005)
-                
-                rotate_motor0(69, 0.005)
-                rotate_motor0(-69, 0.005)
-                
-                rotate_motor(250, 0.005)
-                time.sleep(4)
-                stored_frame = frame.copy()
-                ret, stored_frame = cap.read()
-                stored_frame = cv2.resize(stored_frame, (640, 480))
-                
-                cv2.imshow("Frame", stored_frame)
-                cv2.waitKey(1)
-                for _ in range(3):
-                    
-                    cap.read()
+        pd = cr.getPrediction(frame)  # Get object classification prediction
 
-            if cv2.waitKey(1) & 0xFF == 27:
-                
-                break
+        cv2.imshow("Frame", frame)  # Display the video frame
+
+        cv2.waitKey(1)  # Refresh the OpenCV window
+
+        if pd[1] != 5:  # If a different object is recognized
+            if pd[1] == 0:  # Classify object as 0
+                # Perform motor movements based on the classification
+                rotate_motor(-130, 0.005)
+                rotate_motor0(69, 0.005)
+                rotate_motor0(-69, 0.005)
+                rotate_motor(130, 0.005)
+                time.sleep(4)  # Sleep for 4 seconds
+
+                # Capture and display stored frame
+                ret, stored_frame = cap.read()
+                stored_frame = cv2.resize(stored_frame, (640, 480))
+                cv2.imshow("Frame", stored_frame)
+                cv2.waitKey(1)
+
+                for _ in range(3):
+                    cap.read()  # Read frames to clear buffer
+
+            # Other classification cases (1, 2, 3, 4) can be handled similarly
+
+        if cv2.waitKey(1) & 0xFF == 27:  # If the 'Esc' key is pressed, exit the loop
+            break
 
 except KeyboardInterrupt:
     pass
- 
 
 # Clean up GPIO settings when the program exits
-
 GPIO.cleanup()
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
-c
